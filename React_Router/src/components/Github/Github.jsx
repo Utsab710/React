c3633e3ca71d8350d6e38338909a851c69b4f5d5
Utsab <utsabsshrestha710@gmail.com>
2024-02-09 15:53:53 +0545

    changes

--- React_Router/src/components/Github/Github.jsx
+++ React_Router/src/components/Github/Github.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
-
+import { useLoaderData } from "react-router-dom";
 function Github() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://api.github.com/users/Utsab710")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      });
-  }, []);
+  const data = useLoaderData();
+  // // const [data, setData] = useState([]);
+  // // useEffect(() => {
+  // //   fetch("https://api.github.com/users/Utsab710")
+  // //     .then((response) => response.json())
+  // //     .then((data) => {
+  // //       console.log(data);
+  // //       setData(data);
+  // //     });
+  // }, []);
   return (
     <div className="text-center m-5 bg-gray-700 text-white p-4 text-3xl">
       Github Followers: {data.followers}
@@ -19,3 +20,8 @@ function Github() {
 }
 
 export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/Utsab710");
+  return response.json();
+};
