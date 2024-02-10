59ffce46df6182a0119a69857473735c8bcaa7a9
Utsab <utsabsshrestha710@gmail.com>
2024-02-10 19:30:03 +0545

    ERROR TAILWILD

---
+++ Themeswitcher09/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-blue-500 text-white ">
+        Thiis a Tailwind CSS component.
+      </div>
+    </>
+  );
+}
+
+export default App;
